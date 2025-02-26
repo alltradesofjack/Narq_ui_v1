426289d83749f9f15a7fcc60fc737d3e8a3ea3d9
Jack Rosenberg <jackrosenberg@Jacks-MBP.JBRLaw.local>
2025-02-26 09:08:23 -0500

    Fixed Payroll Selection page

---
+++ src/components/PayrollProviderList.js
@@ -0,0 +1,13 @@
+export default function PayrollProviderList() {
+    return (
+      <div className="flex flex-col items-center gap-4">
+        <p>Mock payroll providers list:</p>
+        <ul className="list-disc">
+          <li>ADP</li>
+          <li>Gusto</li>
+          <li>Paychex</li>
+        </ul>
+      </div>
+    );
+  }
+  
